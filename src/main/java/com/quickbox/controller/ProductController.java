43268780d29bfacc8187ccbe8e3e7b2fb2892efc
Vishaln_2004 <vishalitisthatis2004@gmail.com>
2024-08-08 20:39:18 +0530

    review2

--- src/main/java/com/quickbox/controller/ProductController.java
+++ src/main/java/com/quickbox/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class ProductController {
 
     @Autowired
